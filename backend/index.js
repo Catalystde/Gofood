1e6ec838e24106dc8c7865dfdc7a5be953d75595
Catalystde <110329965+Catalystde@users.noreply.github.com>
2023-09-03 14:32:29 +0530

    hjjh

--- backend/index.js
+++ backend/index.js
@@ -8,10 +8,9 @@ mongoDB();
 const cors = require('cors');
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("strict-origin-when-cross-origin");
   next();
 })
 app.get('/', (req, res) => {
