46a71b5fe720166128866767d5c75e86e68fe280
Catalystde <110329965+Catalystde@users.noreply.github.com>
2023-09-03 14:16:53 +0530

    dcdaasc

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ mongoDB();
 const cors = require('cors');
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "");
+  res.setHeader("Access-Control-Allow-Origin", "https://gofood-api.vercel.app/");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("strict-origin-when-cross-origin");
