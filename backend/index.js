16196dc912ff44393f951ef69ea841a1216cf7e3
Catalystde <110329965+Catalystde@users.noreply.github.com>
2023-09-03 12:44:51 +0530

    d

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("strict-origin-when-cross-origin");
   next();
 })
 app.get('/', (req, res) => {
