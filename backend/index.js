2cb3052feb20223544ab7865387dd16e9eb04279
Catalystde <110329965+Catalystde@users.noreply.github.com>
2023-09-03 12:41:28 +0530

    jjj

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(cors({
     methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 }));
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://gofood-api.vercel.app/");
+  res.setHeader("Access-Control-Allow-Origin", "");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
